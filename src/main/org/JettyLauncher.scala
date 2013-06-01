7640c0eee35d3f4807542982c6e3b2f2fc5bdd1e
Alex Domeshek <alex.domeshek@xplenty.com>
2013-06-02 00:13:17 +0300

    class=>object

--- src/main/org/JettyLauncher.scala
+++ src/main/org/JettyLauncher.scala
@@ -5,7 +5,7 @@ import org.eclipse.jetty.servlet.{ DefaultServlet, ServletContextHandler }
 import org.eclipse.jetty.webapp.WebAppContext
 
 
-class JettyLauncher {
+object JettyLauncher {
     def main(args: Array[String]) {
         val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
